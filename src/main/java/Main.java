1e3c34caff6386167740da6d440338916a036d23
IveriNarozashvili <geobmw91>
2020-04-20 19:26:00 +0300

    update code

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+class Main {
+
+    public static void main(String[] args) {
+       int a = 15;
+       int b = 10;
+       if (a>b) System.out.println("a bolshe chem b");
+       else System.out.println("b boolshe chem aK");
+    }
+}
